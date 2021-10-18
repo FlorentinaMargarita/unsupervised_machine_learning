6574e50ca9bad26f947b26b2b1664451ebd9b4c3
FloTheFlower <florentina.hofbauer@gmail.com>
2021-10-18 05:29:09 -0700

    add clean

--- fit.py
+++ fit.py
@@ -4,6 +4,23 @@ import matplotlib.pyplot as plt
 import json
 import re
 
+
+def clean(s):
+    # remove non-ASCII characters
+    s = s.encode('ascii', errors='ignore').decode()
+    # remove punctuation
+    s = s.translate(str.maketrans('', '', string.punctuation))
+    # convert tabs and newlines to spaces
+    s = re.sub('[\n\t]', ' ', s)
+    # remove leading spaces
+    s = re.sub('^\s+', '', s)
+    # remove trailing spaces
+    s = re.sub('\s+$', '', s)
+    # squeeze multiple spaces into one
+    s = re.sub(r'\s\s+', ' ', s)
+    return s
+
+
 def load_json_data(path, n):
     with open(path, 'rb') as json_file:
         for i, line in enumerate(json_file):
