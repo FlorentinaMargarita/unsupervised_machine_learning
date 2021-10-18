94518c2e35e993246fb49652a842f6e3d56b20a9
FloTheFlower <florentina.hofbauer@gmail.com>
2021-10-18 05:30:21 -0700

    add clean, add more comments

--- fit.py
+++ fit.py
@@ -21,14 +21,33 @@ def clean(s):
     return s
 
 
+# read the JSON data file and yield the relevant columns
+# for each line (metadata record).
 def load_json_data(path, n):
     with open(path, 'rb') as json_file:
+        # for each line in json_file, line number "i"
         for i, line in enumerate(json_file):
+            # we were only asked to read n lines, so if we've read more,
+            # we should stop.
             if i >= n: return
+            # if we're at a multiple of 50k lines, let's print a progress
+            # message with the number of lines read and what percent of
+            # the total number of records it is.
             if i % 50000 == 0: print(f'{100*i/1955945.0:.0f}% line {i}')
+            # parse the line as JSON.  "paper" is now a dict with the
+            # paper metadata.
             paper = json.loads(line)
+            # each metadata entry has a list of versions: when it was first
+            # uploaded, and then version entries for any subsequent changes.
+            # we use the year of the first version (upload) as the date
+            # of the paper.
+            # the "created" field has a full timestamp, e.g.
+            # Tue, 24 Jul 2007 20:10:27 GMT
+            # so we find the year by looking for any four digits.
             year = int(re.search(r'\d{4}', paper['versions'][0]['created']).group())
-            yield year, paper['title'], paper['categories'], paper['abstract']
+            # clean (handle non-ASCII characters, punctuation, and whitespace)
+            # the title and abstract, and yield all the fields of interest.
+            yield year, clean(paper['title']), paper['categories'], clean(paper['abstract'])
 
 for row in load_json_data(r'C:\Users\flore\Downloads\WPy64-3950\notebooks\arxiv-metadata-oai-snapshot.json', 5):
     print(row) 
